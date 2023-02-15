e02554fb4753e97ad65c6aaf0034d15b4a39c5a4
utnesz <90829509+utnesz@users.noreply.github.com>
2023-02-15 18:31:21 +0100

    Routing done, components are working

--- Beteghivo/src/app/app-routing.module.ts
+++ Beteghivo/src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CallerScreenComponent } from './common/caller-screen/caller-screen.component';
+import { LocalScreenComponent } from './common/local-screen/local-screen.component';
+import { MainScreenComponent } from './common/main-screen/main-screen.component';
+import { NextNrScreenComponent } from './common/next-nr-screen/next-nr-screen.component';
+import { HomeScreenComponent } from './page/home-screen/home-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeScreenComponent,
+  },
+  {
+    path: 'caller',
+    component: CallerScreenComponent,
+  },
+  {
+    path: 'localScreen',
+    component: LocalScreenComponent,
+  },
+  {
+    path: 'mainScreen',
+    component: MainScreenComponent,
+  },
+  {
+    path: 'nextNrScreen',
+    component: NextNrScreenComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Beteghivo/src/app/app.module.ts
+++ Beteghivo/src/app/app.module.ts
@@ -1,30 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BetegComponent } from './common/beteg/beteg.component';
 import { NextNrScreenComponent } from './common/next-nr-screen/next-nr-screen.component';
 import { MainScreenComponent } from './common/main-screen/main-screen.component';
 import { LocalScreenComponent } from './common/local-screen/local-screen.component';
 import { CallerScreenComponent } from './common/caller-screen/caller-screen.component';
 import { HomeScreenComponent } from './page/home-screen/home-screen.component';
+import { NavigationComponent } from './common/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BetegComponent,
     NextNrScreenComponent,
     MainScreenComponent,
     LocalScreenComponent,
     CallerScreenComponent,
-    HomeScreenComponent
+    HomeScreenComponent,
+    NavigationComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
