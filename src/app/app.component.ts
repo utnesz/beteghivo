6c5f21cd6df88ded94b6fe51abd4efdf00f82b98
utnesz <90829509+utnesz@users.noreply.github.com>
2023-02-19 19:36:48 +0100

    still working on post request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Beteghivo';
+  constructor() {}
+
+  // ngOnInit(): void {}
+  // title = 'Beteghivo';
+  //
+  // onBtnPost(postData: { title: string; content: //string });
 }
