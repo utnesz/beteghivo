a77f5f6dabb86a5b8b915e6672d9de7a37320af8
utnesz <90829509+utnesz@users.noreply.github.com>
2023-02-21 21:44:49 +0100

    it should work, but it isn't...

--- src/app/services/exam.service.ts
+++ src/app/services/exam.service.ts
@@ -1,4 +1,3 @@
-import { Sorszam } from './../model/sorszam';
 import { Vizsgalat } from '../model/vizsgalat';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
